17ea10ad458d37a2a85a3436022ce21819c87fdc
thdudgus <gusthdud527@cau.ac.kr>
2024-11-07 22:02:22 +0900

    docs : TODO 구현 위한 주석 작성

--- ai-assignment1-skeleton/clustering.py
+++ ai-assignment1-skeleton/clustering.py
@@ -58,8 +58,12 @@ def kmeans(X, max_iterations, k):
     # max_iterations: 최대 반복 횟수
     # k: 클러스터의 개수
 
+    # k를 정하는 방법 : Elbow-method : k를 증가시키고, 각 k에 대한 objective function을 관찰.
     # (TODO) Write your code here
-    
+    # 1.각 cluster의 중심을 data point 집합 D 내의 임의의 위치로 초기화 (cluster 중심 랜덤 초기화)
+    # 2. 각 데이터 포인트 x_i에 대해, 가장 가까운 cluster 찾기. (x_i가 어디에 속하나)
+    # 3. cluster 중심 업데이트
+    # 2~3을 cluster의 중심점이 더이상 변하지 않거나, 최대 반복횟수에 도달할 때까지 반복
 
     # 반환 파라미터
     # centroids: 클러스터 중심점
@@ -74,6 +78,11 @@ def gmm(X, max_iterations, k):
     # k: 클러스터의 개수
 
     # (TODO) Write your code here
+    # 1. 각 데이터 포인트 x에 대해 해당 포인트가 각 cluster에 속할 확률 계산.
+    # 2. parameter 업데이트
+    # 1~2를 parameter의 변화가 매우 작아지거나 최대 반복 횟수에 도달했을 때까지 반복.
+
+    # parameter는 EM 알고리즘을 사용해서 estimate
 
     # 반환 파라미터
     # centroids: 클러스터 중심점
@@ -88,6 +97,15 @@ def dbscan(X, eps, min_pts):
     # min_pts: 최소 데이터 포인트 수 
 
     # (TODO) Write your code here
+    # 초기화 : 각 데이터 포인트에 대해 아직 분류되지 않은 상태로 시작
+    # 각 데이터 포인트 i에 대해 다음 단계 수행
+    # 1. o가 cluster에 할당되지 않은 경우
+        # 1-1. 데이터가 core-object인지 확인
+            # 1-1-1. 밀도 도달 가능한 객체를 모은다.
+            # 1-1-2. 새로운 cluster에 할당
+        # 1-2. core-object가 아니라면
+            #1-2-1. 해당 포인트를 noise로 할당.
+    # 위 과정을 모든 데이터 포인트가 cluster에 할당되거나 noise로 분류될 때까지 반복.
 
     # 반환 파라미터
     # memberships: a list of memberships for data points(각 데이터 포인트의 클러스터 할당)
